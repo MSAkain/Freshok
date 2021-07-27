37b98bf71034596036c114e139ce778007066671
MSAkain <theggodisthere@gmail.com>
2021-07-27 17:05:10 +0300

    dz-1

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(function () {
+	$('.top-slider__inner').slick({
+		prevArrow: '<button type="button" class="slick-arrow arrow-right"><img src="images/icon/arrow-left.svg" alt="Стрелка влево"></button>',
+		nextArrow: '<button type="button" class="slick-arrow arrow-left"><img src="images/icon/arrow-right.svg" alt="Стрелка влево"></button>'
+	});
+
+	var mixer = mixitup('.content');
+
+});
