342517e14d7d17baf479c98add159cde457f0dad
MSAkain <theggodisthere@gmail.com>
2021-07-30 11:33:04 +0300

    added 3 section and fix bugs

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,18 @@ $(function () {
 		nextArrow: '<button type="button" class="slick-arrow arrow-left"><img src="images/icon/arrow-right.svg" alt="Стрелка влево"></button>'
 	});
 
-	var mixer = mixitup('.content');
+
+	var containerEl1 = document.querySelector('[data-ref="container-1"]');
+	var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+	var config = {
+		controls: {
+			scope: 'local'
+		}
+	};
+
+	var mixer1 = mixitup(containerEl1, config);
+	var mixer2 = mixitup(containerEl2, config);
 
 });
+
